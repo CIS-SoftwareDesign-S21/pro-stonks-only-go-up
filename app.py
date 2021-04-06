fbb8b927fb1edaf491c08c68a631532847801b67
Steven Zhou <venturizhou@gmail.com>
2021-04-05 23:24:20 -0400

    fixed up webpage conducting nltk on sentiment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import numpy as np
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 import reddit_scraper
@@ -11,14 +12,14 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv(
-    r'GME.csv')
+# df = pd.read_csv(
+#     r'GME.csv')
 
-fig = go.Figure()
+# fig = go.Figure()
 
-fig.add_trace(go.Scatter(x=df['Date'],y=df['Close'],
-                        mode = 'lines',
-                        name = 'GME'))
+# fig.add_trace(go.Scatter(x=df['Date'],y=df['Close'],
+#                         mode = 'lines',
+#                         name = 'GME'))
 
 app.layout = html.Div(children=[
     html.H1(children='Stonks only go Up'),
@@ -33,16 +34,16 @@ app.layout = html.Div(children=[
     #     placeholder="input type text",
     # ),
     dcc.RadioItems(
+        id='Stonks',
         options=[
             {'label': 'Gamestop', 'value': 'GME'},
             {'label': 'Tesla', 'value': 'TSLA'},
         ],
-        value='MTL',
+        value='GME',
         labelStyle={'display': 'inline-block'}
     ),
     dcc.Graph(
-        id='GME Historical',
-        figure=fig
+        id='Historical',
     ),
     html.H2(children='Recent Relevant Post Titles (250)'),
     html.Button('Query Reddit for Selected Stock Posts', id='update_titles', n_clicks=0),
@@ -59,6 +60,49 @@ app.layout = html.Div(children=[
         id='gme_titles_listbox'
     )
 ])
+
+@app.callback(Output('Historical', 'figure'),
+              [Input('Stonks', 'value')])
+def render_charts(stonk):
+
+    df = pd.read_csv(
+        r'GME.csv')
+
+    df2 = pd.read_csv(
+        r'TSLA.csv')
+
+    # charts = [
+    #     go.Scatter(labels=df['Date'], values=df2['Close'],
+    #                mode='lines', name='GME'),
+    #     go.Scatter(labels=df2['Date'], values=df2['Close'],
+    #                mode='lines', name='TSLA'),
+    # ]
+
+    fig = go.Figure()
+    fig2 = go.Figure()
+
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Close'],
+                             mode='lines',
+                             name='GME'))
+
+    fig.add_trace(go.Scatter(x=df2['Date'], y=np.random.rand(len(df['Date']))*df['Close'].max(),
+                             mode='lines',
+                             name='Sentiment',
+                             opacity=0.3))
+
+    fig2.add_trace(go.Scatter(x=df2['Date'], y=df2['Close'],
+                              mode='lines',
+                              name='TSLA'))
+
+    fig2.add_trace(go.Scatter(x=df2['Date'], y=np.random.rand(len(df['Date']))*df2['Close'].max(),
+                             mode='lines',
+                             name='Sentiment',
+                             opacity=0.3))
+
+    if stonk == 'GME':
+        return fig
+    elif stonk == 'TSLA':
+        return fig2
 
 @app.callback(
     dash.dependencies.Output('gme_titles_listbox', 'children'),
