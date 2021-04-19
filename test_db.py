a15cefe00170598bb31451584e4a0369f086d616
Steven <venturizhou@gmail.com>
2021-04-19 13:03:39 -0400

    some minor changes to post editing

--- test_db.py
+++ test_db.py
@@ -1,9 +1,18 @@
 import dbconn
-
+import reddit_scraper
+import pandas as pd
 
 # f = open('gme_titles.txt', 'r', encoding='utf8')
 # for line in f:
 #     dbconn.insert_reddit_post('GME', line.strip())
 # print("DONE")
 
-print(dbconn.get_reddit_posts('TSLA'))
+# print(dbconn.get_reddit_posts('TSLA'))
+# print(reddit_scraper.search_pushshift_titles_content_only('GME',1,0))
+
+df = pd.DataFrame(reddit_scraper.search_pushshift_titles_content_only('GME',1,0),columns = ['Title','Context','Timestamp'])
+print(df.head())
+# df2 = pd.DataFrame(reddit_scraper.search_pushshift_titles('GME',1,0),columns = ['Title','Context','Timestamp'])
+# print(df2.head())
+
+# reddit_scraper.search_pushshift_titles_content_only('GME',1,0)
